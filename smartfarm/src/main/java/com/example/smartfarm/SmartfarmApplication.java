ca73ef37a4d30ce698b89bf474a8e7f896631c27
TheGreat <mirindano111@gmail.com>
2025-11-06 14:51:42 +0700

    feat: use .env instead using info directly in application

--- smartfarm/src/main/java/com/example/smartfarm/SmartfarmApplication.java
+++ smartfarm/src/main/java/com/example/smartfarm/SmartfarmApplication.java
@@ -3,16 +3,12 @@ package com.example.smartfarm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 @EnableScheduling // THÊM ANNOTATION NÀY
 public class SmartfarmApplication {
 
 	public static void main(String[] args) {
-		// Load environment variables from .env file
-		Dotenv dotenv = Dotenv.configure().load();
-		dotenv.entries().forEach(entry -> System.setProperty(entry.getKey(), entry.getValue()));
 
 		SpringApplication.run(SmartfarmApplication.class, args);
 	}
